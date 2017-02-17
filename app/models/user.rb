c51edb56b265ae278cfbaa4c714f966761dfcfe8
Ivan Kelber <ivankelber@gmail.com>
2017-02-16 21:10:39 -0500

    added passwords to user model

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
+  $VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  $VALID_PASSWORD_REGEX = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{7,}$/
+  validates(:name,presence:true,length:{maximum:50})
+  validates(:email,presence:true,length:{maximum:255},
+            format: {with:$VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false});
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 7 },
+            format: {with:$VALID_PASSWORD_REGEX, multiline:true}
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/create'
+
+  get 'users/destroy'
+
+  get 'users/update'
+
+  get 'users/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'skeleton#home'
 

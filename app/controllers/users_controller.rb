42c2cc9bd61e31852fe7442492d0634a5943014a
Ivan Kelber <ivankelber@gmail.com>
2017-02-19 21:20:52 -0500

    added basic admin log in and rights

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :show]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: [:destroy, :index]
 
 
   def new
@@ -17,13 +18,16 @@ class UsersController < ApplicationController
     if @user.save
       # Handle a successful save.
       flash[:success] = "Welcome to #{Administration::SITE_NAME}!"
-      redirect_back_or user
+      redirect_back_or @user
     else
       render 'new'
     end
   end
 
   def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   def update
@@ -70,5 +74,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless @user == current_user
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end

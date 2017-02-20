8da37feff42032e495759110ff7ad04267146063
Ivan Kelber <ivankelber@gmail.com>
2017-02-19 20:19:52 -0500

    added cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
      if user && user.authenticate(params[:session][:password])
        # Log the user in and redirect to the user's show page.
+       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
        log_in user
+       remember user
        redirect_to user
      else
        # Create an error message.
@@ -19,6 +21,13 @@ class SessionsController < ApplicationController
   def destroy
     log_out
     redirect_to root_path
+  end
+
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
 end

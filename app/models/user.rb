b5ed0b1d00f8e8f72f0ba5d791ca0b31cf6f7a29
Ivan Kelber <ivankelber@gmail.com>
2017-02-19 22:20:15 -0500

    removed need for symbol in password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
 
   $VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  $VALID_PASSWORD_REGEX = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{7,}$/
+  $VALID_PASSWORD_REGEX = /^(?=.*?[a-z])(?=.*?[0-9]).{7,}$/
   validates(:name,presence:true,length:{maximum:50})
   validates(:email,presence:true,length:{maximum:255},
             format: {with:$VALID_EMAIL_REGEX},

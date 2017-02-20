ef3bec4028f2df6cb20da8ecdfed19a88b05546d
Ivan Kelber <ivankelber@gmail.com>
2017-02-19 20:30:08 -0500

    removed hard coded instance of app_name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Survivor Party"
+    base_title = Administration::SITE_NAME
     if page_title.empty?
       base_title
     else

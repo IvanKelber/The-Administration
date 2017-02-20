9af9d8a7e83e9e34d3ae2d56f6df499c3513541f
Ivan Kelber <ivankelber@gmail.com>
2017-02-20 18:01:02 -0500

    schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20170220161909) do
     t.string   "remember_digest"
     t.boolean  "admin",             default: false
     t.string   "activation_digest"
-    t.string   "string"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
     t.string   "reset_digest"

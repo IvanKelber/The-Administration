bb2e22c838da4b63be06931c0d443fee9875b22b
Ivan Kelber <ivankelber@gmail.com>
2017-02-20 21:51:14 -0500

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,29 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170221010628) do
 
-  create_table "components", force: :cascade do |t|
-    t.string   "type"
-    t.text     "description"
-    t.integer  "max_teams"
-    t.integer  "min_teams"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "keywords", force: :cascade do |t|
-    t.string   "word"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "supplies", force: :cascade do |t|
-    t.string   "item"
-    t.integer  "component_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.index ["component_id"], name: "index_supplies_on_component_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"

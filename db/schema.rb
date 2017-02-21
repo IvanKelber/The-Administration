65e82a15d982c014ff12e6c3c989449c088276cf
Ivan Kelber <ivankelber@gmail.com>
2017-02-20 21:22:19 -0500

    changed schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220161909) do
+ActiveRecord::Schema.define(version: 20170221010628) do
+
+  create_table "components", force: :cascade do |t|
+    t.string   "type"
+    t.text     "description"
+    t.integer  "max_teams"
+    t.integer  "min_teams"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "keywords", force: :cascade do |t|
+    t.string   "word"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "supplies", force: :cascade do |t|
+    t.string   "item"
+    t.integer  "component_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["component_id"], name: "index_supplies_on_component_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"

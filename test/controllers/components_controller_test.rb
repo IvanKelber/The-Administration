cdb553b756bc966797ccea6a3fa835f057dd3b28
Ivan Kelber <ivankelber@gmail.com>
2017-02-21 23:42:22 -0500

    added admin access to components test

--- test/controllers/components_controller_test.rb
+++ test/controllers/components_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class ComponentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @glick = users(:glick)
+    @admin = users(:admin)
+  end
+
+  test "non-admin can't get components" do
+    log_in_as @glick
+    get components_path
+    assert_redirected_to root_path
+    get new_component_path
+    assert_redirected_to root_path
+  end
+
+  test "admin can get components" do
+    log_in_as @admin
+    get components_path
+    assert_response :success
+    get new_component_path
+    assert_response :success
+  end
+
 end

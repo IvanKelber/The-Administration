ec2e9104004a686fbffd155c9cd71406c9a6c802
Ivan Kelber <ivankelber@gmail.com>
2017-02-21 22:14:10 -0500

    Added some components tests.  Added some extra validators for components: min_teams <= max_teams, category in (Alcohol,Physical,Puzzle)

--- app/models/component.rb
+++ app/models/component.rb
@@ -4,12 +4,23 @@ class Component < ApplicationRecord
   has_many :keywords, through: :component_keyword_joins
   has_many :component_supply_joins
   has_many :supplies, through: :component_supply_joins
+  $VALID_CATEGORIES = ["Alcohol", "Physical", "Puzzle"]
 
-
-  validates :name, presence:true
+  validates :name, presence:true,  length:{maximum:50}
   validates :description, presence:true, length:{maximum:1000}
-  validates :category, presence:true
+  validates :category, presence:true, inclusion: {in: $VALID_CATEGORIES}
   validates :min_teams, presence:true, numericality:{only_integer:true,greater_than:0,less_than:5}
   validates :max_teams, presence:true, numericality:{only_integer:true,greater_than:0,less_than:5}
+  validate :min_less_than_max
 
+
+  private
+
+    def min_less_than_max
+      if self.min_teams && self.max_teams
+        if self.min_teams > self.max_teams
+          errors.add(:min_teams,"can't be greater than max_teams")
+        end
+      end
+    end
 end
--- test/models/component_test.rb
+++ test/models/component_test.rb
@@ -1,7 +1,80 @@
 require 'test_helper'
 
 class ComponentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @component = components(:chug)
+  end
+
+  test "is valid" do
+    assert @component.valid?
+  end
+
+  test "name presence" do
+    @component.name = "   "
+    assert !@component.valid?
+  end
+
+  test "name not too long" do
+    @component.name = "a" * 51
+    assert !@component.valid?
+  end
+
+  test "description presence" do
+    @component.description = "   "
+    assert !@component.valid?
+  end
+
+  test "description not too long" do
+    @component.description = "a" * 10001
+    assert !@component.valid?
+  end
+
+  test "category presence" do
+    @component.category = " "
+    assert !@component.valid?
+  end
+
+  test "valid category accepted" do
+    invalid_categories = ["Alcohol","Physical","Puzzle"]
+    invalid_categories.each do |category|
+      @component.category = category
+      assert @component.valid?, "Category: #{category}"
+    end
+  end
+
+  test "invalid category rejected" do
+    invalid_categories = ["alcohol","physical","puzzle","something else"]
+    invalid_categories.each do |category|
+      @component.category = category
+      assert !@component.valid?
+    end
+  end
+
+  test "min_teams valid" do
+    @component.min_teams = nil
+    assert !@component.valid?
+    @component.min_teams = 0
+    assert !@component.valid?
+    @component.min_teams = 5
+    assert !@component.valid?
+  end
+
+  test "max_teams valid" do
+    @component.max_teams = nil
+    assert !@component.valid?
+    @component.max_teams = 0
+    assert !@component.valid?
+    @component.max_teams = 5
+    assert !@component.valid?
+  end
+
+  test "max_teams greater than or equal to min_teams" do
+    @component.min_teams = 4
+    @component.max_teams = 1
+    assert !@component.valid?
+    @component.max_teams = @component.min_teams
+    assert @component.valid?
+  end
+
 end

2c298d6e57c9caabb427cb245d873c8b1556a3f2
Ivan Kelber <ivankelber@gmail.com>
2017-02-21 23:35:24 -0500

    added validation for being logged in to see media as well as appropriate tests

--- app/controllers/skeleton_controller.rb
+++ app/controllers/skeleton_controller.rb
@@ -1,5 +1,5 @@
 class SkeletonController < ApplicationController
-  
+  before_action :logged_in_user, only:[:media]
 
   def home
   end
--- test/controllers/skeleton_controller_test.rb
+++ test/controllers/skeleton_controller_test.rb
@@ -2,29 +2,37 @@ require 'test_helper'
 
 class SkeletonControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @base_title = Administration::SITE_NAME
+    @user = users(:glick)
+  end
+
   test "should get root" do
     get root_path
     assert_response :success
-  end
-
-  test "should get media" do
-    get media_path
-    assert_response :success
-  end
-
-  test "should get rules" do
-    get rules_path
-    assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
-  test "should get contact" do
-    get contact_path
+  test "should get rules" do
+    get rules_path
     assert_response :success
+    assert_select "title", "Rules | #{@base_title}"
+  end
+
+  test "should get media only if logged in" do
+    get media_path
+    assert_redirected_to login_url
+
+    log_in_as @user
+    get media_path
+    assert_response :success
+    assert_select "title", "Media | #{@base_title}"
   end
 
 end

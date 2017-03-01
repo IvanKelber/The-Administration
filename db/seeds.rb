b30235710b272565d74fc53d3ec9a08c1c3e5643
Ivan Kelber <ivankelber@gmail.com>
2017-03-01 16:44:14 -0500

    everything working.  also added admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name:"Ivan Kelber",email:"ivankelber@gmail.com",password_digest:User.digest("password1"),admin:true,activated:true)
